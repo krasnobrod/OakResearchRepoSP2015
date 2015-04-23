70d4630e2af578e03b08d443defad226e4577232
krasnobrod <skrasnobrod@mail.csuchico.edu>
2015-04-23 15:30:04 -0700

    v1 data into GitHub, begin R file

---
+++ Jessica_Data Management .R
@@ -0,0 +1,7 @@
+# Getting started
+# Started: April 24, 2015. Begin to assemble data, and improve legibility 
+library(xlsx)
+datav1 <- read.xlsx("/Users/SamKrasnobrod/GitHub/OakResearchRepoSP2015/ChicoPhenv1.xlsx", 1) #Samuel
+datav1 <- read.xlsx("/GitHub/OakResearchRepoSP2015/ChicoPhenv1.xlsx", 1) #Robin                                                      
+##########################################################################################################################                                                      
+str(datav1) #Check composition for numerical values
