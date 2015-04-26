451a4b150e6b7e16251377953848db36eb6314d6
krasnobrod <skrasnobrod@mail.csuchico.edu>
2015-04-26 10:04:44 -0700

    Code w/ v2

--- Jessica_Data Management .R
+++ Jessica_Data Management .R
@@ -1,7 +1,7 @@
 # Getting started
 # Started: April 24, 2015. Begin to assemble data, and improve legibility 
 library(xlsx)
-datav1 <- read.xlsx("/Users/SamKrasnobrod/GitHub/OakResearchRepoSP2015/ChicoPhenv1.xlsx", 1) #Samuel
-datav1 <- read.xlsx("/GitHub/OakResearchRepoSP2015/ChicoPhenv1.xlsx", 1) #Robin                                                      
+datav2 <- read.xlsx("/Users/SamKrasnobrod/GitHub/OakResearchRepoSP2015/ChicoPhenv2.xlsx", 1) #Samuel
+datav2 <- read.xlsx("/GitHub/OakResearchRepoSP2015/ChicoPhenv2.xlsx", 1) #Robin                                                      
 ##########################################################################################################################                                                      
-str(datav1) #Check composition for numerical values
+str(datav2) #Check composition for numerical values
