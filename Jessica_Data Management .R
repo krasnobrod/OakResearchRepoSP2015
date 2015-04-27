8e4bac288cbd82dccb2f86fcc3f1220f8be2e0b2
Robin Jeffries <rjeffries@csuchico.edu>
2015-04-27 14:09:08 -0700

    Read in and create new vars
    Read in the data and create two new variables for bud burst and full
    leaves. NOTE: Due to the way read.xlsx works you have to specify the
    last row number when reading in the file.

--- Jessica_Data Management .R
+++ Jessica_Data Management .R
@@ -1,7 +1,30 @@
 # Getting started
 # Started: April 24, 2015. Begin to assemble data, and improve legibility 
 library(xlsx)
-datav2 <- read.xlsx("/Users/SamKrasnobrod/GitHub/OakResearchRepoSP2015/ChicoPhenv2.xlsx", 1) #Samuel
-datav2 <- read.xlsx("/GitHub/OakResearchRepoSP2015/ChicoPhenv2.xlsx", 1) #Robin                                                      
+
+setwd("/Users/SamKrasnobrod/GitHub/OakResearchRepoSP2015") # Samuel
+setwd("C:/GitHub/OakResearchRepoSP2015") # Robin
+
 ##########################################################################################################################                                                      
-str(datav2) #Check composition for numerical values
+
+# This step takes a while to read in. 
+datav2 <- read.xlsx("ChicoPhenv2.xlsx", sheetIndex=1, header=TRUE, rowIndex = c(1, 3:1000), colIndex=c(1:22))     
+
+# Add a count of days from planting until bud burst (First recorded 1)
+bb <- rep(NA, NROW(datav2))
+for(i in 1:NROW(datav2)){
+    x <- min(which(datav2[i,c(11:20)]==1))*7
+    bb[i] <- ifelse(x<Inf, x, NA)
+}
+datav2$budburst <- bb
+  
+# Add a count of days from planting until full leafs (First recorded 5)
+fl <- rep(NA, NROW(datav2))
+for(i in 1:NROW(datav2)){
+  x <- min(which(datav2[i,c(11:20)]==5))*7
+  fl[i] <- ifelse(x<Inf, x, NA)
+}
+datav2$days.to.leaf <- fl
+
+
+
