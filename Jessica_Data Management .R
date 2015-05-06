09ccf443cf22e82dcebf60ddc5ee0c0683c5b19f
norcalbiostat <rjeffries@csuchico.edu>
2015-05-06 00:05:54 -0700

    ordered boxplots by latitude

--- Jessica_Data Management .R
+++ Jessica_Data Management .R
@@ -1,17 +1,14 @@
 # Getting started
-# Started: April 24, 2015. Begin to assemble data, and improve legibility 
+# Started: April 24, 2015. Begin to assemble data, and improve legibility
 library(foreign)
 library(lattice)
-
+library(dplyr)
 
 setwd("/Users/SamKrasnobrod/GitHub/OakResearchRepoSP2015") # Samuel
 setwd("C:/GitHub/OakResearchRepoSP2015") # Robin
 
-##########################################################################################################################                                                      
-
-# This step takes a while to read in. 
-#datav2 <- read.xlsx("ChicoPhenv2.xlsx", sheetIndex=1, header=TRUE, rowIndex = c(1, 3:3678), colIndex=c(1:22))     
-datav2 <- read.csv("ChicoPhenv2.csv", header=TRUE, stringsAsFactors=FALSE)     
+##########################################################################################################################
+datav2 <- read.csv("ChicoPhenv2.csv", header=TRUE, stringsAsFactors=FALSE)
 
 weeks <- which(grepl("^week", names(datav2), ignore.case=TRUE))
 
@@ -37,7 +34,7 @@ datav2$full.leaf <- fl
 # - If the site name starts with TAC, then replace it with "TAC" only, otherwise keep original value
 datav2$site <- ifelse(grepl("^TAC", datav2$site, ignore.case=TRUE), "TAC", datav2$site)
 
-# Only keep records with valid site names. 
+# Only keep records with valid site names.
 site.factor <- as.factor(datav2$site) # converted site to a temp factor variable
 bad.levels <- levels(site.factor)[1:13] # listed invalid site names
 `%ni%` <- Negate(`%in%`) #Define a new function that is "not in"
@@ -45,75 +42,88 @@ datav3 <- subset(datav2, datav2$site %ni% bad.levels) #subset the data to only k
 
 
 # merge on lat & long data by tree
+latlong      <- read.csv("Source data copy.csv", header=TRUE,  stringsAsFactors=FALSE)
+latlong$site <- ifelse(grepl("^TAC", latlong$Site.abb., ignore.case=TRUE), "TAC", latlong$Site.abb.)
+ # Create a new Tree ID
+latlong$id <- with(latlong, paste(site ,  Acc..No., Tree.No., sep='-'))
+datav3$id  <- datav3$Use.this.tree
+latlong    <- latlong[,-c(1:4)]
 
-
-# Sort by lat (instead of site)
-
+# merge on matching  variables (site, tree id)
+datav4 <- left_join (datav3, latlong)
+datav4$sitelat <- reorder(datav4$site, datav4$Latitude, min)
+datav4 <- datav4[,-c(5:7)]
 
 
 # clean up
-rm(bb, fl, site.factor, weeks, i, bad.levels, datav2, x);gc()
-
-# Write the data as a text file - and produce SAS code to read in data. 
-write.foreign(datav3, "ChicoPhen.txt", "ReadChicoPhen.sas", package="SAS")
+rm(bb, fl, site.factor, weeks, i, bad.levels, datav2, x, latlong, datav3);gc()
 
 #################################################################################################
 ## Use bb or fl against site in anova
-library(lattice)
-histogram(datav3$budburst) #shows bb for weeks 1-8
-histogram(~full.leaf | site, data=datav3)
 
-# CALCULATE THE MEAN VALUE PER SITE
-mean.bb <- tapply(datav3$budburst, datav3$site, mean, na.rm=TRUE)
-mean.fl <- tapply(datav3$full.leaf, datav3$site, mean, na.rm=TRUE)
-
+histogram(datav4$budburst) #shows bb for weeks 1-8
+histogram(~full.leaf | site, data=datav4)
 
 # OVERALL MEANS - TO BE PLOTTED AS THE VERTICAL LINE
-mean.total.bb <- mean(datav3$budburst, na.rm=TRUE)
-mean.total.fl <- mean(datav3$full.leaf, na.rm=TRUE)
+mean.total.bb <- mean(datav4$budburst, na.rm=TRUE)
+mean.total.fl <- mean(datav4$full.leaf, na.rm=TRUE)
 
-# Calculate the lower and upper quartiles for each variable by site
-q1.bb <- tapply(datav3$budburst, datav3$site, function(x)quantile(x,.25, na.rm=TRUE))
-q3.bb <- tapply(datav3$budburst, datav3$site, function(x)quantile(x,.75, na.rm=TRUE))
-# COPY/PASTE CHANGE BB TO FL
-q1.fl <- tapply(datav3$full.leaf, datav3$site, function(x)quantile(x,.25, na.rm=TRUE))
-q3.fl <- tapply(datav3$full.leaf, datav3$site, function(x)quantile(x,.75, na.rm=TRUE))
-# create a vector of colors, where if the Q1 is above the mean or Q3 is below the mean color it red.
-color.bb <- ifelse(q1.bb>mean.total.bb | q3.bb<mean.total.bb, "red", "gray90")
-#copy/paste and change bb to fl
-color.fl <- ifelse(q1.fl>mean.total.fl | q3.bb<mean.total.fl, "red", "gray90")
+# CALCULATE summary statistics PER SITE
+# Also create a vector of colors, where if the Q1 is above the mean or Q3 is below the mean color it red.
+# -- this is data management code using dplyr. High end novice stage learning, but easier
+# -- sometimes to use.
+bysite <- datav4 %>%
+          group_by(sitelat) %>%
+          summarise(mean.bb = mean(budburst, na.rm=TRUE),
+                    mean.fl = mean(full.leaf, na.rm=TRUE),
+                    q1.bb = quantile(budburst,.25, na.rm=TRUE),
+                    q3.bb = quantile(budburst,.75, na.rm=TRUE),
+                    q1.fl = quantile(full.leaf,.25, na.rm=TRUE),
+                    q3.fl = quantile(full.leaf,.75, na.rm=TRUE),
+                    minlat = round(min(Latitude, na.rm=TRUE), 3)) %>%
+    mutate(color.bb = ifelse(q1.bb>mean.total.bb | q3.bb<mean.total.bb, "red", "gray90"),
+           color.fl = ifelse(q1.fl>mean.total.fl | q3.fl<mean.total.fl, "red", "gray90"))
+
 
 
 #### PLOT BUDBURST DISTRIBUTION BY SITE ###
+y <- NROW(bysite)
 
-png(filename="budburst.png", width=480, height=1100) # get this height looking good before copy 
-
-boxplot(budburst~site, data=datav3, horizontal=TRUE, axes=FALSE, pch=".", col=color.bb, 
+png(filename="budburst.png", width=480, height=1100) # get this height looking good before copy
+par(mar=c(5,4,4,5))
+boxplot(budburst~sitelat, data=datav4, horizontal=TRUE, axes=FALSE,
+        col=bysite$color.bb, cex=.2, ylab="Site",
         main="Week of first Bud burst by site", xlab="weeks")
-axis(2, las=2, at=1:length(mean.bb), label=names(mean.bb), cex.axis=0.28)
+axis(2, las=2, at=1:y, label=bysite$sitelat, cex.axis=0.6)
+axis(4, las=2, at=1:y, label=bysite$minlat,cex.axis=0.6)
+mtext(side=4, line=3, "Latitude")
 axis(1, at=1:10)
 box()
 abline(v=mean.total.bb, col="slateblue", lwd=2)
-points(mean.bb, 1:length(mean.bb), col="blue", pch=16)
+points(bysite$mean.bb, 1:y, col="blue", pch=16)
 
 dev.off()
 
 # Copy/paste all above code and change to fl
-png(filename="full.leaf.png", width=480, height=1100) 
-
-boxplot(full.leaf~site, data=datav3, horizontal=TRUE, axes=FALSE, pch=".", col=color.bb, 
-        main="Week of first Full Leaf by site", xlab="weeks")
-axis(2, las=2, at=1:length(mean.fl), label=names(mean.fl), cex.axis=.28)
+png(filename="full.leaf.png", width=480, height=1100)
+par(mar=c(5,4,4,5))
+boxplot(full.leaf~sitelat, data=datav4, horizontal=TRUE, axes=FALSE,
+        col=bysite$color.fl, cex=.2, ylab="Site",
+        main="Week of first Full leaf by site", xlab="weeks")
+axis(2, las=2, at=1:y, label=bysite$sitelat, cex.axis=0.6)
+axis(4, las=2, at=1:y, label=bysite$minlat,cex.axis=0.6)
+mtext(side=4, line=3, "Latitude")
 axis(1, at=1:10)
 box()
 abline(v=mean.total.fl, col="slateblue", lwd=2)
-points(mean.fl, 1:length(mean.fl), col="blue", pch=16)
+points(bysite$mean.fl, 1:y, col="blue", pch=16)
 
+dev.off()
 
 ######################################################################
 
-summary(aov(budburst~site, data=datav3)) #anova bb against site
-summary(aov(full.leaf~site, data=datav3))
+summary(aov(budburst~site, data=datav4)) #anova bb against site
+summary(aov(full.leaf~site, data=datav4))
 
 
 ## Setup histogram with lattice for anova results
--- ReadChicoPhen.sas
+++ ReadChicoPhen.sas
@@ -1,31 +1,36 @@
-* Written by R;
-*  write.foreign(datav2, "ChicoPhen.txt", "ReadChicoPhen.sas", package = "SAS") ;
+/* Author: Robin Jeffries
+/* Date: 5/5/15
+/* Purpose: Data management for Oak tree phenom data
+******************************************************/
 
-DATA  rdata ;
-LENGTH
- section $ 6
- row $ 2
- TREE $ 16
- ID_notes $ 26
- Use_this_tree $ 16
- site $ 10
- week_9 $ 30
- Notes $ 33
- day $ 7
+
+%let libname = C:\GitHub\OakResearchRepoSP2015;
+libname s "&libname";
+
+
+DATA phen ;
+format
+	section $6.
+ 	row $2.
+ 	TREE $16.
+ 	ID_notes $26.
+ 	Use_this_tree $16.
+ 	site $10.
+ 	Notes $33.
+ 	day $7.
 ;
 
-INFILE  "ChicoPhen.txt" 
-     DSD 
-     LRECL= 141 ;
+INFILE  "ChicoPhenv2.csv"  delimiter=',' missover DSD firstobs=2;
+     
 INPUT
- section
- row
- Line
- SERP
- TREE
- ID_notes
- Use_this_tree
- site
+ section $
+ row $
+ Line $
+ SERP 
+ TREE $
+ ID_notes $
+ Use_this_tree $
+ site $
  acc
  tree_num
  week_1
@@ -38,23 +43,28 @@ INPUT
  week_8
  week_9
  week_10
- Notes
- day
- budburst
- full_leaf
+ Notes $
+ day $
 ;
-LABEL  ID_notes = "ID.notes" ;
-LABEL  Use_this_tree = "Use.this.tree" ;
-LABEL  tree_num = "tree.num" ;
-LABEL  week_1 = "week.1" ;
-LABEL  week_2 = "week.2" ;
-LABEL  week_3 = "week.3" ;
-LABEL  week_4 = "week.4" ;
-LABEL  Week_5 = "Week.5" ;
-LABEL  Week_6 = "Week.6" ;
-LABEL  Week_7 = "Week.7" ;
-LABEL  week_8 = "week.8" ;
-LABEL  week_9 = "week.9" ;
-LABEL  week_10 = "week.10" ;
-LABEL  full_leaf = "full.leaf" ;
-RUN;
+run;
+
+
+data phen2;
+	set phen;
+
+	* Clean up site names;
+	if (index(site, "TAC")>0) then site = "TAC";
+
+	* Add a count of weeks from planting until bud burst (First recorded 1);
+	array b(*) week_1--week_10;
+	bb = .; fl = .; 
+	if week_1>0 then bb=1;		if week_1=5 then fl=1;
+	do i = 2 to dim(b);
+		if (b[i-1]=0 & b[i]>0 ) then bb = i;
+		if (b[i-1]<5 & b[i]=5) then fl = i;
+	end;
+
+	drop i ;
+run;
+
+* Merge with latlong data;
